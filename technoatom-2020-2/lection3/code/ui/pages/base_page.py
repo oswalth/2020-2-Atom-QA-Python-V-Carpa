ddc8f5753d8a84d6aac18919f61ba42c96062e5e
oswalth <oswalth2@gmail.com>
2020-10-05 18:34:50 +0300

    lecture3 added

---
+++ technoatom-2020-2/lection3/code/ui/pages/base_page.py
@@ -0,0 +1,38 @@
+class MainPage(object):
+    def find(self, locator, timeout=None) -> WebElement:
+        # нотация WebElement удобна тем, что у метода find становятся доступны методы WebElement
+        # а это очень удобно
+
+        return self.wait(timeout).until(EC.presence_of_element_located(locator))  # поиск элемента с ожиданием
+
+    def click(self, locator, timeout=None):
+        # попытки чтобы кликнуть
+        for i in range(RETRY_COUNT):
+            try:
+                self.find(locator)
+                element = self.wait(timeout).until(EC.element_to_be_clickable(locator))
+                element.click()
+                return
+
+            except StaleElementReferenceException:
+                if i < RETRY_COUNT - 1:
+                    pass
+        raise
+
+    def scroll_to_element(self, element):
+        # нигде не используется, потому что click сам скролит
+        # просто пример возможной реализации
+        self.driver.execute_script('arguments[0].scrollIntoView(true);', element)
+
+    def wait(self, timeout=None):
+        if timeout is None:
+            timeout = 5
+        return WebDriverWait(self.driver, timeout=timeout)
+
+    def count_elements(self, locator, count, timeout=1):
+        # этот метод считает количество элементов на странице
+        # until принимает функцию, а значит мы можем написать и использовать свою, в нашем случае это lambda функция
+        # в этом методе мы ожидаем пока не появится нужное нам количество элементов на странице
+
+        self.wait(timeout).until(lambda browser: len(browser.find_elements(*locator)) == count)
+
